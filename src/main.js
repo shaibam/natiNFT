5bf8b2ad1fa71a95cd96beebfcb7246950aa7fcf
shai kimchi <shai.k@diglobal.tech>
2021-12-19 10:39:19 +0200

    random almost there

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const tree = require('./tree.json')
+//const tree = require('./tree.json')
+const tree = require('./miniTree.json')
 const fs = require('fs');
 const _ = require('lodash');
 //require('deepdash')(_);
@@ -13,22 +14,47 @@ const createVariations = (_tree) => {
     let nextV = [];
     if (slice.length) {
         nextV = createVariations(slice);
-        if (!_tree[0].mandatory)
-            nextV.push(null);
+        //if (!_tree[0].mandatory) {
+        //    console.log(nextV);
+        //    nextV.push(null);
+        //}
     }
 
     //console.log({ slice, files })
     for (var i in files) {
-        if (!nextV.length)
+        if (!nextV.length) {
+            //stuck with 3d glasses
+            console.log({ n: files[i] })
             v.push([_tree[0].folders[0] + '/' + files[i]]);
-        else
+        } else
             for (var j in nextV) {
-                v.push([_tree[0].folders[0] + '/' + files[i]]);
-                v[v.length - 1] = v[v.length - 1].concat(nextV[j])
+                const r = Math.random();
+                //console.log({ r, n: files[i] })
+                if (_tree[0].mandatory || r < .1) {
+                    v.push([_tree[0].folders[0] + '/' + files[i]]);
+                    v[v.length - 1] = v[v.length - 1].concat(nextV[j])
+                } else {
+                    v.push([null]);
+                    v[v.length - 1] = v[v.length - 1].concat(nextV[j])
+                }
             }
     }
     return v
 }
+
+const variations = createVariations(tree);
+console.log({ variationsL: variations.length });
+const nv = _.values(_.keyBy(_.map(variations, _.compact)));
+console.log()
+console.log({ nvL: nv.length });
+
+fs.writeFile("./log/log.json", JSON.stringify(nv), function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+
 
 const ffmpegVariation = (variation, outputFile) => {
     //ffmpeg -i B.png -i A.png -filter_complex "[1]scale=iw/2:-1[b];[0:v][b] overlay" out.png
@@ -36,7 +62,6 @@ const ffmpegVariation = (variation, outputFile) => {
     variation.forEach((fileName) => command.mergeAdd(fileName));
     const complexFilter = _.map(variation.slice(0, variation.length - 1), (a, i) => (`[${i}]overlay${i < variation.length - 2 ? `[${i + 1}]` : ''}`))
 
-    console.log(complexFilter)
     command
         .complexFilter(complexFilter)
         .output(outputFile)
@@ -45,7 +70,6 @@ const ffmpegVariation = (variation, outputFile) => {
             console.log(`cannot process the video ${err.message}`);
         });
 }
-const variations = createVariations(tree);
 
 var j = Math.floor(Math.random() * variations.length);
 for (var i = 0; i < 10; i++) {
@@ -54,4 +78,3 @@ for (var i = 0; i < 10; i++) {
     j = Math.floor(Math.random() * variations.length)
 }
 
-console.log({ variationsL: variations.length });
